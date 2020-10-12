00b694defba18d3946416f7f9d65369179943e2b
Nandini <nandini.siva17@gmail.com>
2020-10-12 18:14:07 +0800

    initial commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.TEST
+{
+    public class UnitTest1
+    {
+        // WeatherForecastController controller = new WeatherForecastController();
+        // [Fact]
+        // public void GetReturnsMyName()
+        // {
+        //     var returnValue = controller.Get(1);
+        //     Assert.Equal("Nandini", returnValue.Value);
+        // }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
